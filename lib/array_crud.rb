994e7d6f659b0dc8f771bf25a66ec32393c490ec
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-11-08 16:31:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Arsenal", "Liverpool", "Chelsea", "West Ham"]
+  ["Arsenal", "Liverpool", "Chelsea", "West HI"]
 end
 
 def add_element_to_end_of_array(array, element)
